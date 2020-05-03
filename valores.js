49a45834bd78f8df5d24634acd78e79fb688e37d
JulianCode <hijuliansosa@protonmail.com>
2020-05-03 01:35:35 -0500

    Elementos de un Lenguaje de Programación: Variables, Funciones y Sintaxis

---
+++ valores.js
@@ -0,0 +1,26 @@
+/*
+  Elementos de un lenguaje de programación
+
+  1 - Data: que guardamos en memoria
+  2 - Tareas (funciones) que hacemos con esa data
+
+*/
+
+// Tipos de datos (data)
+
+// Valores Primitivos
+
+40 // Number
+
+'Valentina' // String
+
+true // Boolean
+false
+
+Undefined  // Valores vacíos
+null
+
+// Valores No primitivos o tipo objeto
+
+[2013, 17, 2] // Arreglo
+{} // Objeto
