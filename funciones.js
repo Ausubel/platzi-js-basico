7236ab70c8b5e6e52b745c38a2deefa191814f0a
JulianCode <hijuliansosa@protonmail.com>
2020-05-03 01:57:59 -0500

    Funciones

---
+++ funciones.js
@@ -0,0 +1,19 @@
+/*
+  Conjunto de sentencías que nosotros podemos utilzar para
+  generar ciertas acciones con los valores que ya guardamos
+  antes en las variables.
+*/
+
+// Existen 2 Tipos de funciones: Declarativas y de Expresión
+
+
+// Declarativas
+  function miFuncion() {
+    return 3;
+  }
+
+// Expresión
+
+var miFuncion = function() {
+  return a + b;
+}
