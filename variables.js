25b4809605620461857e636a0abfe00cc4114f4b
JulianCode <hijuliansosa@protonmail.com>
2020-05-03 01:44:11 -0500

    Variables

---
+++ variables.js
@@ -0,0 +1,13 @@
+/*
+  - Es la representación de algun lugar en memoria que vamos a reservar para guardar un valor
+  - var es la palabra reservada de JS para entender que es una variable
+  - ESTADOS DE UNA VARIABLE:
+      -DECLARAR
+      -INICIALIZAR
+*/
+
+var nombre; // declarar
+
+nombre // undefined, debido a que aún no hay valor, no inicializada
+
+nombre = 'Valentina'; // inicializar
