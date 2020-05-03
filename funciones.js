fca1c5f062912f05b523e35df4fd5920f8346ce3
JulianCode <hijuliansosa@protonmail.com>
2020-05-03 02:03:39 -0500

    Funciones, diferencias, link completo

--- funciones.js
+++ funciones.js
@@ -17,3 +17,18 @@
 var miFuncion = function() {
   return a + b;
 }
+
+/** 
+  // Diferencias:
+
+  A las funciones declarativas se les aplica hoisting, y a la expresión de función, no.
+  Ya que el hoisting solo se aplica en las palabras reservadas var y function.
+
+  Lo que quiere decir que con las funciones declarativas, podemos mandar llamar la función
+  antes de que ésta sea declarada, y con la expresión de función, no,
+  tendríamos que declararla primero, y después mandarla llamar.
+
+ */
+
+// https://www.campusmvp.es/recursos/post/que-es-el-hoisting-en-javascript.aspx
+// https://platzi.com/clases/1814-basico-javascript/26295-cuando-utilizar-una-funcion-declarativa-y-una-expr/
