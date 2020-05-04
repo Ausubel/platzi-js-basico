b079549659d1bb4d1e32e86eb01a2e27e00b1185
JulianCode <hijuliansosa@protonmail.com>
2020-05-04 14:30:44 -0500

    Operadores: Asignación, Comparación y Aritméticos.

---
+++ operadores.js
@@ -0,0 +1,50 @@
+// Operadores: Asignación, Comparación y Aritméticos.
+
+// Operadores Binarios o Aritmeticos
+// Binarios porque hace 1 operación entre 2 valores
+3 + 2
+50 - 10
+10 * 20
+20 / 2 
+
+"Valentina" + "Sosa" // El operador + también es operador de concatenación
+
+
+// Unity Operators - Tenemos un solo operador trabajando con un solo valor
+
+  ! // Negación
+  !false // es true porque esta negando el valor
+
+// Operadores de asignación
+// =
+
+var a = 1; // "Asignamos" un valor a una variable
+
+// Operadores de comparación
+3 == "3"; // Compara el valor
+3 === "3"; // Compara el valo y el tipo
+5 < 3; // falso
+5 > 3; // verdad
+5 <= 6; // verdad
+5 >= 6; // falso
+
+// Validar si ambos valores son iguales (&&) o si alguno es verdad (||)
+a && b; // Validar si ambas son verdaderas
+a || b; // Validar si una o otra es verdadera
+
+
+// Incrementar
+var edad = 40;
+
+edad++; // Incrementa a 41
+++edad; // Incrementa a 41
+
+edad += 2 // Incrementa edad a 42
+
+// Decrementar
+var edad = 40;
+
+edad--; // decrementa a 39
+--edad; // decrementa a 39
+
+edad -= 2 // decrementa edad a 38
