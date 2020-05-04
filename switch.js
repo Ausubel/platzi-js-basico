f807d82017d6a1a18ea27a0c41135bdbbd760f09
JulianCode <hijuliansosa@protonmail.com>
2020-05-04 16:24:57 -0500

    Switch

---
+++ switch.js
@@ -0,0 +1,21 @@
+var elCaso = 1;
+
+switch (elCaso) {
+  case 1:
+    console.log('Soy uno');
+    break;
+  case 2:
+    console.log('Soy dos');
+    break;
+  case 3:
+    console.log('Soy tres');
+    break;
+  case 4:
+    console.log('Soy cuatro');
+    break;
+  case 5:
+    console.log('Soy cinco');
+    break;
+  default:
+    console.log('Soy otro número');
+}
