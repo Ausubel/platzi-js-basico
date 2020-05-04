735b80a57dd2348b75470c449aa3bcdd324849c0
JulianCode <hijuliansosa@protonmail.com>
2020-05-04 17:34:56 -0500

    Objects

---
+++ objects.js
@@ -0,0 +1,11 @@
+// Objetos
+
+// los objetos se manejan von propiedades
+// las propiedades es una combinación de palabras clave y valores
+var miAuto = {
+  marca: 'Range Rover',
+  modelo: 'Velar',
+  annio: '2020',
+};
+
+miAuto[marca]; // En este momento estamos accediendo al valor de la marca: 'Range Rover'
